0279f772e0a0b76020ddbc196548269bb2d5cde1
Giga Gatenashvili <g.gatenashvili@gmail.com>
2019-10-22 17:19:29 +0400

    removed unneded files, added cmd argument handlig to executor.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ const fs = require('fs');
 const readline = require('readline');
 const christofides = require('./christofides');
 
-const points = [];
-
-for (let i = 0; i < 20; i++) {
-    points.push([Math.random() * 15, Math.random() * 15])
-}
+const [
+    inputFile='input.txt',
+    outputFile='output.json'
+] = process.argv.slice(2);
 
 async function readPointsFile(file) {
     const points = [];
@@ -17,24 +16,26 @@ async function readPointsFile(file) {
             })
         }).on('line', line => {
             const point = line.split(',');
-            if(point.length === 2) {
+            if (point.length === 2) {
                 points.push([parseFloat(point[0]), parseFloat(point[1])]);
             }
         }).on('close', () => resolve(points));
     });
 }
 
-readPointsFile('sample.points').then(points => {
+readPointsFile(inputFile).then(points => {
     console.info("Start computation...");
 
     const [tour, distance, executionTimeMs] = christofides(points);
 
-    const result =JSON.stringify({
+    const result = JSON.stringify({
         shortest_path_description: tour,
         shortest_path_size: distance,
         time_of_execution: (executionTimeMs / 1000) + ' seconds',
     }, null, 4);
 
-    fs.writeFileSync('result.json', result);
+    fs.writeFileSync(outputFile, result);
 
 }).catch(e => console.log('Error while reading input file', e));
+
+
